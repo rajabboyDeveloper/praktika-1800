ba37cace7951555f0c54d9f265cee7089dd6942a
Codecraft <codecraftuz@INHA.local>
2023-12-16 18:57:34 +0500

    dobavil map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import { YMaps ,Map ,Placemark,FullscreenControl,TrafficControl,RouteButton} from 'react-yandex-map'
 function App() {
   return (
     <div className=''>
-      <h1>hello </h1>
-      <h2>dsadsadsa</h2>
+<YMaps>
+    <Map  defaultState={{
+    center: [41.338663, 69.334790],
+    zoom: 13
+  }}>
+      <Placemark geometry={[41.338663, 69.334790]} />
+      <FullscreenControl />
+      <TrafficControl options={{
+      float: 'right'
+    }} />
+    <RouteButton options={{
+      float: 'right'
+    }} />
+    </Map>
+  </YMaps>
     </div>
   )
 }
